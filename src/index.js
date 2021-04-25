dfdc311580a7e91b3b6209ddf07fa03358fe4142
KonstHardy <konst.hardy@gmail.com>
2021-04-25 18:28:41 +0300

    [add] store & reducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { rootReducer } from "./store/rootReducer";
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/store/rootReducer.js
@@ -0,0 +1,21 @@
+const initialState = [
+  {
+    id: "kimZX28vZHo",
+    alt: "Prety dog",
+    src:
+      "https://images.unsplash.com/photo-1544568104-5b7eb8189dd4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxODc3NDJ8MHwxfHJhbmRvbXx8fHx8fHx8&ixlib=rb-1.2.1&q=80&w=1080",
+  },
+  {
+    id: "kimZX28vZH1",
+    alt: "Prety dog",
+    src:
+      "https://images.unsplash.com/photo-1544568104-5b7eb8189dd4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxODc3NDJ8MHwxfHJhbmRvbXx8fHx8fHx8&ixlib=rb-1.2.1&q=80&w=1080",
+  },
+];
+
+export const rootReducer = (state = initialState, action) => {
+  // default:
+  //     return state;
+
+  return state;
+};
