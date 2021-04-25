198196bddef507b63ed2b01c1261176874b0e484
KonstHardy <konst.hardy@gmail.com>
2021-04-26 01:19:27 +0300

    [add] PhotoList
    [add] PhotoList component;

--- src/components/photoList/index.js
+++ src/components/photoList/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./photoList.css";
 
-const PhotoList = () => {
+const PhotoList = (props) => {
+  const { photos } = props;
+  // console.log(photos);
+
   return (
     <div className="photoList">
-      <div className="wrapper photoList__wrapper">PhotoList</div>
+      <div className="wrapper photoList__wrapper">
+        {photos.map((photo) => (
+          <div className="photo-card" key={photo.id}>
+            <img
+              className="photo-card__image"
+              alt={photo.alt}
+              src={photo.src}
+            ></img>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Search />
-      <PhotoList />
+      <PhotoList photos={photos} />
     </>
   );
 }
