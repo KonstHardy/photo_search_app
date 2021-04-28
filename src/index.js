e9677810ea1e63ea103681cbf3672cc2042e0635
KonstHardy <konst.hardy@gmail.com>
2021-04-29 01:34:05 +0300

    [fix] store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./index.css";
 import App from "./containers/App";
-
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { rootReducer } from "./store/rootReducer";
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
   <Provider store={store}>
