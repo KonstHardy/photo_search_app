99ed76d328893c5243f4608784e2727d6935c344
KonstHardy <konst.hardy@gmail.com>
2021-04-30 14:40:17 +0300

    [fix] structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { Provider } from "react-redux";
-import store from "./store";
 
 import "./index.css";
+
 import App from "./containers/App";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
