b8c240ee7d17bf3b46bfc09b8de76d4b121a5219
KonstHardy <konst.hardy@gmail.com>
2021-05-07 18:48:24 +0300

    [fix] api

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
-import { createApi } from "unsplash-js";
+import Unsplash from "unsplash-js";
 
-const API_URL = "https://api.unsplash.com";
-const ACCESS_KEY = "V7Sr52HbltODrFjnlVkvTO3gIh50m4CeGsLozyTFE1E";
+const APP_ACCESS_KEY = "V7Sr52HbltODrFjnlVkvTO3gIh50m4CeGsLozyTFE1E";
+// const API_URL = "https://api.unsplash.com";
 
-export const Unsplash = createApi({
-  accessKey: ACCESS_KEY,
-  apiUrl: API_URL,
+const unsplash = new Unsplash({
+  accessKey: APP_ACCESS_KEY,
+  // apiUrl: API_URL,
 });
+
+export default unsplash;
