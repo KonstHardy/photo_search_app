75b5a654ea48e4a213b1d9467b8365ebd742a9d0
KonstHardy <konst.hardy@gmail.com>
2021-05-11 00:06:20 +0300

    [fix] action auth

--- src/store/actions/auth/index.js
+++ src/store/actions/auth/index.js
@@ -3,7 +3,7 @@ import unsplash from "../../../api";
 
 import { GET_ACCESS_KEY } from "../../types";
 
-export const authenticationUrl = unsplash.auth.getAuthenticationUrl([
+const authenticationUrl = unsplash.auth.getAuthenticationUrl([
   "public",
   "read_user",
   "write_user",
@@ -11,18 +11,41 @@ export const authenticationUrl = unsplash.auth.getAuthenticationUrl([
   "write_photos",
 ]);
 
-// location.assign(authenticationUrl);
+// Пример 1
 
-export function loginAction(code) {
+// const code = window.location.search.split("code=")[1];
+
+// export function loginAction() {
+//   return async (dispatch) => {
+//     await unsplash.auth
+//       // .userAuthentication(query.code)
+//       .userAuthentication(code)
+//       .then(toJson)
+//       .then((json) => {
+//         console.log("GET_ACCESS_KEY", json);
+//         unsplash.auth.setBearerToken(json.access_token);
+//         dispatch({ type: GET_ACCESS_KEY, payload: json.results });
+//       });
+//   };
+// }
+
+// Пример 2
+
+export function loginAction() {
+  const code = window.location.search.split("code=")[1];
+
   return async (dispatch) => {
-    await unsplash.auth
-      // .userAuthentication(query.code)
-      .userAuthentication(code)
-      .then(toJson)
-      .then((json) => {
-        console.log("GET_ACCESS_KEY", json);
-        unsplash.auth.setBearerToken(json.access_token);
-        dispatch({ type: GET_ACCESS_KEY, payload: json.results });
-      });
+    if (code) {
+      unsplash.auth
+        .userAuthentication(code)
+        .then((res) => res.json())
+        .then((json) => {
+          // Сохраняем полученный токен
+          unsplash.auth.setBearerToken(json.access_token);
+
+          console.log("GET_ACCESS_KEY", json);
+          dispatch({ type: GET_ACCESS_KEY, payload: json.access_token });
+        });
+    }
   };
 }
