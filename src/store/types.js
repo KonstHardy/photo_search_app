fca9fda1c9c48b5d00e90b92336147d4119cb1d8
KonstHardy <konst.hardy@gmail.com>
2021-05-16 02:16:20 +0300

    [add] constants

--- src/store/types.js
+++ src/store/types.js
@@ -1,4 +1,9 @@
 export const SEARCH_PHOTOS = "SEARCH_PHOTOS";
 export const FETCH_PHOTOS = "FETCH_PHOTOS";
 
+// export const CURRENT_PAGE = "CURRENT_PAGE";
+
+export const SHOW_LOADER = "SHOW_LOADER";
+export const HIDE_LOADER = "HIDE_LOADER";
+
 export const GET_ACCESS_KEY = "GET_ACCESS_KEY";
