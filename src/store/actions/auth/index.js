bf3cc3ca9feb2e5ccc7363341a9d73f84645bc98
KonstHardy <konst.hardy@gmail.com>
2021-05-18 20:41:52 +0300

    [fix] authentication

--- src/store/actions/auth/index.js
+++ src/store/actions/auth/index.js
@@ -3,19 +3,23 @@ import unsplash from "../../../api";
 
 import { GET_ACCESS_KEY } from "../../types";
 
-// const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-//   "public",
-//   "read_user",
-//   "write_user",
-//   "read_photos",
-//   "write_photos",
-// ]);
+const authenticationUrl = unsplash.auth.getAuthenticationUrl([
+  "public",
+  "read_user",
+  "write_user",
+  "read_photos",
+  "write_photos",
+]);
 
-// // location.assign(authenticationUrl);
+// window.location.assign(authenticationUrl);
 
 const code = window.location.search.split("code=")[1];
 
 export function loginAction() {
+  // window.location.assign(authenticationUrl);
+
+  // const code = window.location.search.split("code=")[1];
+
   return async (dispatch) => {
     await unsplash.auth
       .userAuthentication(code)
@@ -23,6 +27,7 @@ export function loginAction() {
       .then((json) => {
         unsplash.auth.setBearerToken(json.access_token);
 
+        // window.location.assign(authenticationUrl);
         localStorage.setItem("BearerToken", json.access_token);
 
         dispatch({ type: GET_ACCESS_KEY });
