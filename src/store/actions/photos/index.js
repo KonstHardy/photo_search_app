242bc433155469a49667e3daf4d1d1a2214b61ef
KonstHardy <konst.hardy@gmail.com>
2021-05-21 12:26:38 +0300

    [add] current page

--- src/store/actions/photos/index.js
+++ src/store/actions/photos/index.js
@@ -1,26 +1,28 @@
 import { toJson } from "unsplash-js";
 import unsplash from "../../../api";
 
-import { SEARCH_PHOTOS, FETCH_PHOTOS } from "../../constants/photos";
+import {
+  SEARCH_PHOTOS,
+  FETCH_PHOTOS,
+  CURRENT_PAGE,
+} from "../../constants/photos";
 
-let pageCounter = 1;
+// let pageCounter = 1;
 
-export function searchPhotos(searchQuery) {
+export function searchPhotos(searchQuery, page) {
   return async (dispatch) => {
     await unsplash.search
-      .photos(searchQuery, pageCounter++, 11)
+      .photos(searchQuery, page, 11)
       .then(toJson)
       .then((json) => {
         console.log("SEARCH_PHOTOS", json);
         dispatch({ type: SEARCH_PHOTOS, payload: json.results });
       });
     // .catch((error) => {
-    //   console.log(
-    //     "Encountered an error with fetching and parsing data",
-    //     error
-    //   );
+    //   console.log("Encountered an error with fetching and parsing data", {
+    //     error,
+    //   });
     // });
-    // return pageCounter++;
   };
 }
 
@@ -33,5 +35,23 @@ export function fetchPhotos() {
         console.log("FETCH_PHOTOS", response.results);
         dispatch({ type: FETCH_PHOTOS, payload: response.results });
       });
+    // .catch((error) => {
+    //   console.log("Encountered an error with fetching and parsing data", {
+    //     error,
+    //   });
+    // });
   };
 }
+
+export function setPage() {
+  return {
+    type: CURRENT_PAGE,
+  };
+}
+
+// export function setPage(page) {
+//   return {
+//     type: CURRENT_PAGE,
+//     payload: page++,
+//   };
+// }
--- src/store/reducers/photos/index.js
+++ src/store/reducers/photos/index.js
@@ -12,18 +12,21 @@ const initialState = {
 const photoReducer = (state = initialState, action) => {
   switch (action.type) {
     case SEARCH_PHOTOS:
-      return { ...state, photos: action.payload };
+      return {
+        ...state,
+        photos: action.payload,
+      };
     // return { ...state, photos: [...state.photos, ...action.payload] };
 
     case FETCH_PHOTOS:
       // return { ...state, photos: action.payload };
       return { ...state, photos: [...state.photos, ...action.payload] };
 
-    // case CURRENT_PAGE:
-    //   return {
-    //     ...state,
-    //     currentPage: action.payload,
-    //   };
+    case CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: state.currentPage + 1,
+      };
 
     default:
       return state;
