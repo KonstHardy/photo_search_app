53dbc3f2c9ec08123c868bc16edeedaa6de4f5cf
KonstHardy <konst.hardy@gmail.com>
2021-05-23 15:42:00 +0300

    [fix] photos & current page reducer

--- src/store/reducers/photos/index.js
+++ src/store/reducers/photos/index.js
@@ -12,11 +12,11 @@ const initialState = {
 const photoReducer = (state = initialState, action) => {
   switch (action.type) {
     case SEARCH_PHOTOS:
-      return {
-        ...state,
-        photos: action.payload,
-      };
-    // return { ...state, photos: [...state.photos, ...action.payload] };
+      // return {
+      //   ...state,
+      //   photos: action.payload,
+      // };
+      return { ...state, photos: [...state.photos, ...action.payload] };
 
     case FETCH_PHOTOS:
       // return { ...state, photos: action.payload };
@@ -26,6 +26,7 @@ const photoReducer = (state = initialState, action) => {
       return {
         ...state,
         currentPage: state.currentPage + 1,
+        // currentPage: state.currentPage,
       };
 
     default:
