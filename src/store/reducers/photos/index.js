328e8dba3b91478c64c8e4f2782eb1f5d8b3d86c
KonstHardy <konst.hardy@gmail.com>
2021-05-25 15:51:20 +0300

    [fix] photoReducer

--- src/store/reducers/photos/index.js
+++ src/store/reducers/photos/index.js
@@ -2,13 +2,10 @@ import {
   SEARCH_PHOTO,
   FETCH_SEARCH,
   FETCH_RANDOM,
-  SET_CURRENT_PAGE,
-  RESET_CURRENT_PAGE,
 } from "../../constants/photos";
 
 const initialState = {
   photos: [],
-  currentPage: 1,
 };
 
 const photoReducer = (state = initialState, action) => {
@@ -20,28 +17,10 @@ const photoReducer = (state = initialState, action) => {
       };
 
     case FETCH_SEARCH:
-      // return {
-      //   ...state,
-      //   photos: action.payload,
-      // };
       return { ...state, photos: [...state.photos, ...action.payload] };
 
     case FETCH_RANDOM:
-      // return { ...state, photos: action.payload };
       return { ...state, photos: [...state.photos, ...action.payload] };
-
-    case SET_CURRENT_PAGE:
-      return {
-        ...state,
-        // currentPage: state.currentPage + 1,
-        currentPage: action.payload,
-      };
-
-    case RESET_CURRENT_PAGE:
-      return {
-        ...state,
-        currentPage: action.payload,
-      };
 
     default:
       return state;
