d37e64671c71ed895c9af342c754aead5ecae4e3
KonstHardy <konst.hardy@gmail.com>
2021-05-25 16:10:53 +0300

    [add] counterPage action

---
+++ src/store/actions/counterPage/index.js
@@ -0,0 +1,20 @@
+import {
+  SET_CURRENT_PAGE,
+  RESET_CURRENT_PAGE,
+} from "../../constants/counterPage";
+
+let pageCounter = 1;
+
+export function setCurrentPage(page) {
+  return {
+    type: SET_CURRENT_PAGE,
+    payload: page + 1,
+  };
+}
+
+export function resetCurrentPage() {
+  return {
+    type: RESET_CURRENT_PAGE,
+    payload: pageCounter,
+  };
+}
