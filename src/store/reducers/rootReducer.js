e4c1efee7380dbc2eae044b9374dd810a3872190
KonstHardy <konst.hardy@gmail.com>
2021-05-25 15:48:50 +0300

    [add] counterPage reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import photos from "./photos";
 import loader from "./loader";
+import counter from "./counterPage";
 
 export default combineReducers({
   auth,
   photos,
   loader,
+  counter,
 });
