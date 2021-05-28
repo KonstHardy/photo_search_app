f0697c397f90e24797d51f0fdcb590ddabc1af16
KonstHardy <konst.hardy@gmail.com>
2021-05-29 01:58:28 +0300

    [add] loader

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,21 +3,25 @@ import { useSelector, useDispatch } from "react-redux";
 
 import "./App.css";
 
-import Search from "./../components/search";
-import PhotoList from "./../components/photoList";
+import Loader from "../components/loader";
+import Search from "../components/search";
+import PhotoList from "../components/photoList";
 
 import { loginAction } from "../store/actions/auth";
 
 function App() {
-  const isAuth = useSelector((state) => state.auth.isAuth);
+  const isAuth = useSelector((state) => state.auth.authentication);
+  const dispatch = useDispatch();
 
   console.log("isAuth", isAuth);
-
-  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loginAction());
   }, []);
+
+  if (!isAuth) {
+    return <Loader />;
+  }
 
   return (
     <>
