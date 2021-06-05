c8c4b0691fae1c69f97e05a1fdda5bf427adf069
KonstHardy <konst.hardy@gmail.com>
2021-06-05 14:19:01 +0300

    [add] notFound component

---
+++ src/components/notFound/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./notFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="wrapper notFound">
+      <p>Sorry, nothing matched your search :(</p>
+      <p>Please try again with some different keywords.</p>
+    </div>
+  );
+};
+
+export default NotFound;
