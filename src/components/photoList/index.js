c8ee5419bb6a599356a173a95d8042a3b525d7c4
KonstHardy <konst.hardy@gmail.com>
2021-06-06 02:02:38 +0300

    [add] notFound for photos.length

--- src/components/photoList/index.js
+++ src/components/photoList/index.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import "./photoList.css";
 
 import PhotoCard from "../photoCard";
+import NotFound from "../notFound";
 import Loader from "../loader";
 
 const PhotoList = () => {
@@ -12,11 +13,16 @@ const PhotoList = () => {
 
   return (
     <div className="photoList">
-      <div className="wrapper photoList__wrapper">
-        {photos.map((photo) => (
-          <PhotoCard photo={photo} key={photo.id} />
-        ))}
-      </div>
+      {photos.length > 0 ? (
+        <div className="wrapper photoList__wrapper">
+          {photos.map((photo) => (
+            <PhotoCard photo={photo} key={photo.id} />
+          ))}
+        </div>
+      ) : (
+        <NotFound />
+      )}
+
       {loading ? <Loader /> : null}
     </div>
   );
