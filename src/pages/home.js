e44e7ebdab652c7ca267c8b1199429087f7b2168
KonstHardy <konst.hardy@gmail.com>
2021-06-08 03:53:18 +0300

    [fix] page home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+import "./home.css";
+
+import SearchTitle from "../components/searchTitle";
+
 const Home = () => {
   return (
-    <div className="wrapper">
-      <h1>Welcome to Home Page</h1>
+    <div className="home">
+      <div className="home__title">
+        <div className="wrapper home__wrapper">
+          <SearchTitle />
+          <p className="home__text">
+            This application allows you to view and download beautiful free
+            images and photos from all&nbsp;over the world.
+          </p>
+        </div>
+      </div>
+      <div className="home__desc">
+        <div className="wrapper desc__wrapper">
+          <p className="desc__text">
+            To use application you need to log in. <br />
+            If you don’t have an account yet, you can create one.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
