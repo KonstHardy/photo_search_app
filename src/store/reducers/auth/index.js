a469d55817b6a76976e1a99a3417e2f3d76b0182
KonstHardy <konst.hardy@gmail.com>
2021-06-09 16:25:39 +0300

    [fix] authReducer

--- src/store/reducers/auth/index.js
+++ src/store/reducers/auth/index.js
@@ -2,13 +2,11 @@ import { GET_ACCESS_KEY } from "../../constants/auth";
 
 const initialState = {
   authentication: false,
-  // isAuth: localStorage.getItem("BearerToken"),
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ACCESS_KEY: {
-      // return { ...state, isAuth: action.payload };
       return { ...state, authentication: true };
     }
 
