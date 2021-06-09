9fd43bf9122d366bd58bd476f8f814f1c1b7aed9
KonstHardy <konst.hardy@gmail.com>
2021-06-09 16:26:25 +0300

    [fix] photoLike

--- src/components/iconLike/index.js
+++ src/components/iconLike/index.js
@@ -15,18 +15,11 @@ function IconLike(props) {
       width={17}
       height={17}
       onClick={() => {
-        {
-          photoLike
-            ? dispatch(removeLike(photoId))
-            : dispatch(addLike(photoId));
-        }
-
-        // dispatch(addLike(photoId));
-        // dispatch(removeLike(photoId));
+        photoLike ? dispatch(removeLike(photoId)) : dispatch(addLike(photoId));
       }}
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
       />
     </svg>
