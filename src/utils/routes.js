1d9a448b96acfe1af10a72b7552a11d01c906731
KonstHardy <konst.hardy@gmail.com>
2021-06-11 21:10:12 +0300

    [add] gallery & login routes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,10 +1,23 @@
 import React from "react";
+
 import Home from "../pages/home";
+import Gallery from "../pages/gallery";
+import Login from "../pages/login";
 
 export default [
   {
     path: "/",
     exact: true,
     component: () => <Home />,
+  },
+  {
+    path: "/gallery",
+    exact: true,
+    component: () => <Gallery />,
+  },
+  {
+    path: "/login",
+    exact: true,
+    component: () => <Login />,
   },
 ];
