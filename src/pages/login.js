94e681c7a4fbe27eefc720ec8a0d010f154771f5
KonstHardy <konst.hardy@gmail.com>
2021-06-14 21:27:47 +0300

    [add] auth for login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import Loader from "../components/loader";
+import { loginAction } from "../store/actions/auth";
 
 const Login = () => {
-  return (
-    <div className="wrapper">
-      <h1>Welcome to Login Page</h1>
-    </div>
-  );
+  const isAuth = useSelector((state) => state.auth.authentication);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loginAction());
+  }, []);
+
+  if (!isAuth) {
+    return <Loader />;
+  }
+
+  return <div className="wrapper">{/* <h1>Welcome to Login Page</h1> */}</div>;
 };
 
 export default Login;
