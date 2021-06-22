6cc8d98cd6ff450d95e624012ecf7cde068bef9c
KonstHardy <konst.hardy@gmail.com>
2021-06-22 17:25:17 +0300

    [add] authRoute

---
+++ src/utils/authRoute.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function AuthRoute(props) {
+  const isAuth = useSelector((state) => state.auth.authentication);
+
+  if (isAuth) {
+    return <Route {...props} />;
+  }
+  // return <Redirect to="/login" />;
+  return <Redirect to="/" />;
+}
