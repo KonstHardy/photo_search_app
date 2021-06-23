91f52872564c94cd2d879d1222096b1f5c652da3
KonstHardy <konst.hardy@gmail.com>
2021-06-23 12:57:01 +0300

    [add] AuthRoute

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,42 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  // Link,
-  // useRouteMatch,
-  // useParams
-} from "react-router-dom";
-
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./App.css";
 
 import Header from "../components/header";
 import routes from "../utils/routes";
-
-// import Loader from "../components/loader";
-// import { loginAction } from "../store/actions/auth";
+import AuthRoute from "../utils/authRoute";
 
 function App() {
   const isAuth = useSelector((state) => state.auth.authentication);
-  // const dispatch = useDispatch();
-
   console.log("isAuth", isAuth);
-
-  // useEffect(() => {
-  //   dispatch(loginAction());
-  // }, []);
-
-  // if (!isAuth) {
-  //   return <Loader />;
-  // }
 
   return (
     <Router>
       <Header />
       <Switch>
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.component}
-          />
-        ))}
+        {routes.map((route, index) => {
+          if (route.protected === "isAuth") {
+            return (
+              <AuthRoute
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.component}
+              />
+            );
+          }
+
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          );
+        })}
       </Switch>
     </Router>
   );
