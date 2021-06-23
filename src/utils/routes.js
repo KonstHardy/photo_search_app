ca853df637ef8e9547e3a12c2f3b4ca2ae97a713
KonstHardy <konst.hardy@gmail.com>
2021-06-23 14:39:06 +0300

    [add] protected for routes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -4,20 +4,25 @@ import Home from "../pages/home";
 import Gallery from "../pages/gallery";
 import Login from "../pages/login";
 
-export default [
+const routes = [
   {
     path: "/",
     exact: true,
     component: () => <Home />,
+    protected: null,
   },
   {
     path: "/gallery",
     exact: true,
     component: () => <Gallery />,
+    protected: "isAuth",
   },
   {
     path: "/login",
     exact: true,
     component: () => <Login />,
+    protected: null,
   },
 ];
+
+export default routes;
