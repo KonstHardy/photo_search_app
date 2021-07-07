90a7f53d66a149d38c734fc5cf265de4ae3a75b5
KonstHardy <konst.hardy@gmail.com>
2021-07-07 22:32:05 +0300

    [fix] remove likesReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,14 +2,12 @@ import { combineReducers } from "redux";
 
 import auth from "./auth";
 import photos from "./photos";
-import like from "./like";
 import loader from "./loader";
 import counter from "./counterPage";
 
 export default combineReducers({
   auth,
   photos,
-  like,
   loader,
   counter,
 });
