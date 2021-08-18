e3e045a43307c849c801d92f1c0b0a9883d3d7e3
Konstantin Popov <konst.hardy@gmail.com>
2021-08-18 13:34:26 +0300

    [add] configureStore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 
 import "./index.css";
 
 import App from "./containers/App";
-import store from "./store";
+
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import rootReducer from "./reducers/rootReducer";
+
+export default function configureStore() {
+  const middlewares = applyMiddleware(thunk);
+  const composedEnhancers = composeWithDevTools(middlewares);
+
+  const store = createStore(rootReducer, composedEnhancers);
+
+  return store;
+}
