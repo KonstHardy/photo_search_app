96a957b0f006afbc70002ecbc1acd108d63aa036
Konstantin Popov <konst.hardy@gmail.com>
2021-08-19 21:20:19 +0300

    [fix] styles for modalActive

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import "./loader.css";
 
 const Loader = () => {
+  const modalActive = useSelector((state) => state.modal.showModal);
+
   return (
-    <div className="loader">
+    <div className={modalActive ? "loader loaderModal" : "loader"}>
       <div></div>
       <div></div>
       <div></div>
