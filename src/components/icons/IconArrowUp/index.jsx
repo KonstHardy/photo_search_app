257973eebb62b4a29984ef266e18e00a49bdc48a
Konstantin Popov <konst.hardy@gmail.com>
2021-09-12 21:57:59 +0300

    [add] IconArrowUp

---
+++ src/components/icons/IconArrowUp/index.jsx
@@ -0,0 +1,12 @@
+import styles from './IconArrowUp.module.css';
+
+export function IconArrowUp() {
+  return (
+    <svg className={styles.root} width={20} height={20} viewBox="0 0 16 16">
+      <path
+        fillRule="evenodd"
+        d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+      />
+    </svg>
+  );
+}
