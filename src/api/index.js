f7a488c3b905f65f16dec28f4dec6c1a9120d7eb
Konstantin Popov <konst.hardy@gmail.com>
2021-11-14 04:13:57 +0300

    [fix] callback url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import Unsplash from 'unsplash-js';
 
 const APP_ACCESS_KEY = 'V7Sr52HbltODrFjnlVkvTO3gIh50m4CeGsLozyTFE1E';
 const APP_SECRET = '26lh7dwP3S5wT_NCvvrfzEWwEHNid9vcVdTes2MSMEM';
-const CALLBACK_URL = 'https://nifty-joliot-938f87.netlify.app/';
+const CALLBACK_URL = 'https://nifty-joliot-938f87.netlify.app/gallery';
 const USER_BEARER_TOKEN = localStorage.getItem('BearerToken');
 
 const unsplash = new Unsplash({
